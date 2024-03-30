59d2f1afc11855e78f11f16b2948cd1cc68e2e08
USMAN-BABA <162150455+USMAN-BABA@users.noreply.github.com>
2024-03-31 00:32:51 +0500

    Add files via upload

---
+++ 3.Name Cases.ts
@@ -0,0 +1,7 @@
+let myName: string = "Muhammad Usman Khalil";
+
+console.log(myName.toLowerCase());
+
+console.log(myName.toUpperCase());
+
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1).toLowerCase());
